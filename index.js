34e60526c3bcd60e08dc6ef93f8e88148f660017
communal-principle-3181 <aellis19@outlook.com>
2019-05-25 03:34:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function removeFromCart(item) {
   for(var i = 0; i < tempCart.length; i++)
   {
 
-    if(item === Object.keys(tempCart[i].itemName))
+    if(item === Object.keys(tempCart[i]))
     {
       tempCart.splice(i, 1);
       return tempCart;
